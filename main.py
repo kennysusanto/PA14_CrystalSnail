ab3b50fa36047ccfc185c75b7f0db6dddc15ee03
Kenny Susanto <kenny.susanto@rocketmail.com>
2020-04-25 10:14:50 +0700

    window size

--- main.py
+++ main.py
@@ -2,8 +2,17 @@ from tkinter import *
 
 root = Tk()
 
+height = 800
+width = 600
+
+root.geometry(f"{height}x{width}")
+root.resizable(0, 0)
+
+frame = Frame(root)
+frame.pack()
+
 text = StringVar()
-label = Label(root, textvariable = text)
+label = Label(frame, textvariable = text)
 
 text.set("yo mabro")
 
